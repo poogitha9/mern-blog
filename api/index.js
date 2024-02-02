771d7d19cd5bfc09fdd30082b7645409378b9d20
poogitha9 <poojithathaya98@gmail.com>
2024-02-02 12:34:57 +0530

    add functionality to the sign up page

--- api/index.js
+++ api/index.js
@@ -7,31 +7,31 @@ import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
-mongoose
-.connect(process.env.MONGO)
-.then(() => {
-    console.log('MongoDb is connected');
-}).catch(err => {
+mongoose.connect(process.env.MONGO)
+.then ( () =>{
+    console.log('MongoDb is connected')
+})
+.catch((err) =>{
     console.log(err);
 });
-const app = express();
 
+const app = express();
 app.use(express.json());
 
 app.listen(3000, () =>{
-    console.log('server is running on port 3000!');
+    console.log("Server is running on port 3000!");
+
 });
 
-app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
-app.use((err,req,res, next)=> {
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
-    const message = err.message || 'internal server error ';
+    const message = err.message || 'Intenal server error';
     res.status(statusCode).json({
-        success:false,
+        success: false,
         statusCode,
-        message
+        message,
     });
 });
-
